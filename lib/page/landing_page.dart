d0131e12b029a22c74b259dbfc19f9c9114b3ab1
kaleb.silva <kaleb.silva@itbam.org.br>
2022-07-14 18:54:38 -0400

    refactor: divider landing page desktop and mobile

--- lib/page/landing_page.dart
+++ lib/page/landing_page.dart
@@ -3,6 +3,7 @@ import 'package:portfolio_app/page/components/end_drawer_component.dart';
 import 'package:portfolio_app/page/components/header_mobile_component.dart';
 import 'package:portfolio_app/page/components/presentation_component.dart';
 import 'package:portfolio_app/page/components/presentation_mobile_component.dart';
+import 'package:portfolio_app/util/responsive.dart';
 import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 
 import 'components/about_component.dart';
@@ -11,48 +12,80 @@ import 'components/experience_component.dart';
 import 'components/header_component.dart';
 
 class LandingPage extends StatelessWidget {
-  LandingPage({Key? key}) : super(key: key);
+  const LandingPage({Key? key}) : super(key: key);
 
-  final GlobalKey<ScaffoldState> _key = GlobalKey();
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      desktop: LandingPageDesktop(),
+      mobile: LandingPageMobile(),
+    );
+  }
+}
+
+class LandingPageDesktop extends StatelessWidget {
+  LandingPageDesktop({Key? key}) : super(key: key);
   final ItemScrollController itemScrollController = ItemScrollController();
-  static const int screenMobile = 600;
 
   @override
   Widget build(BuildContext context) {
     List<Widget> components = [
-      _generateHeader(),
-      const PresentationMobileComponent(),
+      HeaderComponent(onClick: _scrollTo),
+      const PresentationComponent(),
       const AboutComponent(),
       const AcademicComponent(),
       const ExperienceComponent(),
     ];
 
     return Scaffold(
-        key: _key,
-        backgroundColor: Colors.white,
-        endDrawer: EndDrawerComponent(onClick: _scrollTo),
-        body: ScrollablePositionedList.builder(
-          itemCount: components.length,
-          itemScrollController: itemScrollController,
-          itemBuilder: (context, index) {
-            return components[index];
-          },
-        ));
+      backgroundColor: Colors.white,
+      body: ScrollablePositionedList.builder(
+        itemCount: components.length,
+        itemScrollController: itemScrollController,
+        itemBuilder: (context, index) {
+          return components[index];
+        },
+      ),
+    );
   }
 
-  Widget _generateHeader() {
-    return LayoutBuilder(
-      builder: (_, constraints) {
-        bool isMobileScreen = (constraints.maxWidth <= screenMobile);
-        Widget header = HeaderComponent(onClick: _scrollTo);
+  void _scrollTo(int index) {
+    itemScrollController.scrollTo(
+        index: index,
+        duration: const Duration(seconds: 1),
+        curve: Curves.easeInExpo);
+  }
+}
 
-        if (isMobileScreen) {
-          header = HeaderMobileComponent(
-            onClick: () => _key.currentState?.openEndDrawer(),
-          );
-        }
-        return header;
-      },
+class LandingPageMobile extends StatelessWidget {
+  LandingPageMobile({Key? key}) : super(key: key);
+
+  final GlobalKey<ScaffoldState> _key = GlobalKey();
+
+  final ItemScrollController itemScrollController = ItemScrollController();
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> components = [
+      HeaderMobileComponent(
+        onClick: () => _key.currentState?.openEndDrawer(),
+      ),
+      const PresentationMobileComponent(),
+      const AboutComponent(),
+      const AcademicComponent(),
+      const ExperienceComponent(),
+    ];
+    return Scaffold(
+      key: _key,
+      backgroundColor: Colors.white,
+      endDrawer: EndDrawerComponent(onClick: _scrollTo),
+      body: ScrollablePositionedList.builder(
+        itemCount: components.length,
+        itemScrollController: itemScrollController,
+        itemBuilder: (context, index) {
+          return components[index];
+        },
+      ),
     );
   }
 
