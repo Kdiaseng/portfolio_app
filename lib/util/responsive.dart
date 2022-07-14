4101ab141fe45f503d71bfdcc2f0eab5f8e064b6
kaleb.silva <kaleb.silva@itbam.org.br>
2022-07-14 18:38:42 -0400

    refactor: create widget responsive

---
+++ lib/util/responsive.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class Responsive extends StatelessWidget {
+  const Responsive({Key? key, required this.desktop, required this.mobile})
+      : super(key: key);
+
+  final Widget desktop;
+  final Widget mobile;
+
+
+  static bool isMobile(BuildContext context) {
+    return MediaQuery.of(context).size.width < 800;
+  }
+
+  static bool isTablet(BuildContext context) {
+    return MediaQuery.of(context).size.width >= 800 &&
+        MediaQuery.of(context).size.width < 1200;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth > 800) {
+          return desktop;
+        }
+
+        return mobile;
+      },
+    );
+  }
+}
